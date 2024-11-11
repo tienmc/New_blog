50f25f46ac6fccb2f52a165f74928873165a7e8b
tienmc <90749346+tienmc@users.noreply.github.com.>
2024-11-11 04:41:13 +0000

    test 3

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,12 +1,11 @@
 class BlogPostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
-    def index 
+    def index
         @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
         @pagy, @blog_posts = pagy(@blog_posts, items: 3 )
         rescue Pagy::OverflowError
             redirect_to root_path(page: 1)
-            
             # params[:page] = 1
             # retry
     end
@@ -14,7 +13,7 @@ class BlogPostsController < ApplicationController
     def show
     end
 
-    def new 
+    def new
         @blog_post = BlogPost.new
     end
 
@@ -44,7 +43,7 @@ class BlogPostsController < ApplicationController
         redirect_to root_path
     end
 
-    private 
+    private
     def blog_post_params
         params.require(:blog_post).permit(:title, :content, :cover_image, :video, :published_at)
     end
@@ -53,14 +52,5 @@ class BlogPostsController < ApplicationController
         @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : @blog_post = BlogPost.published.find(params[:id])
         rescue ActiveRecord::RecordNotFound
         redirect_to root_path
-    end
-
-
-    def pagy_get_vars(collection, vars)
-        { page: vars[:page], items: vars[:items] }
-    end
-
-    def pagy_get_items(collection, pagy)
-        collection.limit(pagy.items).offset(pagy.offset)
     end
 end
